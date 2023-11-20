8d4e85c0bdbb1ccdc9c6b7708781bce7f392d3ab
Alex Trotman <alextrotman04@gmail.com>
2023-11-20 14:04:09 -0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <img alt="example alternate text" src=""></img>
+      <h1>Hi, I'm Alex</h1>
+      <h2>About Me</h2>
+      <p>Your biography text goes here</p>
+      <a>GitHub</a>
+      <a>LinkedIn</a>
+    </div>
+  )
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,64 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1,
+      });
+    // Assert
+    expect(topLevelHeading).toBeInTheDocument();
+  });
+
+test('displays and image of myself with alt text', () => {
+    render(<App />)
+
+    const image = screen.getByRole("img")
+
+    expect(image).toBeInTheDocument();
+});
+
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    const secondLevelHeading = screen.getByRole("heading", {
+        name: /About Me/i,
+        exact: false,
+        level: 2,
+      });
+    // Assert
+    expect(secondLevelHeading).toBeInTheDocument();
+  });
+
+  test("A paragraph for your biography", () => {
+    render(<App />)
+
+    const paragraphText = "Your biography text goes here";
+    const paragraph = screen.getByText(paragraphText);
+
+    expect(paragraph).toBeInTheDocument();
+  })
+
+  test("Github link", () => {
+    render(<App />)
+
+    const linkText = "GitHub";
+    const link = screen.getByText(linkText);
+
+    expect(link).toBeInTheDocument();
+  })
+
+  test("LinkedIn link", () => {
+    render(<App />)
+
+    const linkText = "LinkedIn";
+    const link = screen.getByText(linkText);
+
+    expect(link).toBeInTheDocument();
+  })
